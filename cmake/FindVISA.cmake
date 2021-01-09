28b212a93b67e0d87ad6998c6e947e2e27fe6896
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-01-09 01:49:58 +0000

    Find NI-VISA in Linux

--- cmake/FindVISA.cmake
+++ cmake/FindVISA.cmake
@@ -1,14 +1,16 @@
 if(WIN32 AND NOT VISA_DIR)
   set(VISA_DIR "C:\\Program Files (x86)\\IVI Foundation\\VISA\\WinNT")
+  set(WIN32_LIBRARY_SEARCH_HINTS ${VISA_DIR}/lib/msc ${VISA_DIR}/Lib_x64/msc)
 endif()
 
 FIND_PATH(VISA_INCLUDE_DIRS
   NAMES visa.h
-  HINTS ${VISA_DIR}/include ${VISA_DIR}/Include)
+  HINTS ${VISA_DIR}/include /usr/include/ni-visa)
 
+# On openSUSE, CMake seems to seach /usr/lib64 but not /usr/lib/x86_64-linux-gnu, so add those
 FIND_LIBRARY(VISA_LIBRARIES
-  NAMES visa64 visa32
-  HINTS ${VISA_DIR}/lib ${VISA_DIR}/lib/msc ${VISA_DIR}/Lib_x64/msc)
+  NAMES visa visa64 visa32
+  HINTS ${VISA_DIR}/lib /usr/lib/i386-linux-gnu /usr/lib/x86_64-linux-gnu ${WIN32_LIBRARY_SEARCH_HINTS})
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(VISA DEFAULT_MSG VISA_LIBRARIES VISA_INCLUDE_DIRS)
