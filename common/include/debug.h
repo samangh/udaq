684c9db353c73331ea0c9059cca4048733de4d5c
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-03-08 16:58:14 +0000

    Fix common::type_name

--- common/include/debug.h
+++ common/include/debug.h
@@ -15,15 +15,15 @@ template <typename T> constexpr auto type_name() noexcept {
     std::string_view name = "Error: unsupported compiler", prefix, suffix;
 #ifdef __clang__
     name = __PRETTY_FUNCTION__;
-    prefix = "auto type_name() [T = ";
+    prefix = "auto udaq::common::type_name() [T = ";
     suffix = "]";
 #elif defined(__GNUC__)
     name = __PRETTY_FUNCTION__;
-    prefix = "constexpr auto type_name() [with T = ";
+    prefix = "constexpr auto udaq::common::type_name() [with T = ";
     suffix = "]";
 #elif defined(_MSC_VER)
     name = __FUNCSIG__;
-    prefix = "auto __cdecl type_name<";
+    prefix = "auto __cdecl udaq::common::type_name<";
     suffix = ">(void) noexcept";
 #endif
     name.remove_prefix(prefix.size());
