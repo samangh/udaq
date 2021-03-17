e61f1d324855c6de163aa3d1f665a8077d9e2041
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-03-17 09:58:48 +0000

    Clean up main

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,150 +1,188 @@
 #include <stdio.h>
 #include <iostream>
-// #include <format>
+#include <vector>
+
 #include <imgui-wrapper.h>
 
+#include <implot.h>
+#include <cstdlib>
 #include <version.h>
 #include "karla-font.h"
 
-int main(int, char**)
-{
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+
+class MyContext {
+  public:
+    SDL_Window *window;
+    SDL_GLContext gl_context;
+    ImGuiIO io;
+};
+
+void clean_up(MyContext &context) {
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(context.gl_context);
+    SDL_DestroyWindow(context.window);
+    SDL_Quit();
+}
+
+MyContext initialise() {
+    MyContext imgui_context;
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
     {
         std::cout << "Error: " << SDL_GetError() << std::endl;
         exit(-1);
     }
 
-    // Decide GL+GLSL versions
-#ifdef __APPLE__
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = "#version 150";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#else
     // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 130";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
 
     // Create window with graphics context
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("uDAQ",  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
+    imgui_context.window = SDL_CreateWindow("uDAQ",  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    imgui_context.gl_context = SDL_GL_CreateContext(imgui_context.window);
+    SDL_GL_MakeCurrent(imgui_context.window, imgui_context.gl_context);
+
     SDL_GL_SetSwapInterval(1); // Enable vsync
 
     // Initialize OpenGL loader
     if (gl3wInit() != 0)
     {
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-        return 1;
+        exit(-1);
     }
 
-    // Setup Dear ImGui context
+     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-    ImGui::GetIO().Fonts->AddFontFromMemoryCompressedTTF(Karla_compressed_data, karla_compressed_size, 14);
-    io.Fonts->Build();
+    ImPlot::CreateContext();
+   imgui_context.io = ImGui::GetIO();
+      (void)imgui_context.io;
 
-    // Setup Dear ImGui style
+    ImGui::GetIO().Fonts->AddFontFromMemoryCompressedTTF(
+        Karla_compressed_data, karla_compressed_size, 14);
+    imgui_context.io.Fonts->Build();
+
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
     ImGui::GetStyle().WindowRounding = 5.0f;
 
+    
     // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplSDL2_InitForOpenGL(imgui_context.window, imgui_context.gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
+    return imgui_context;
+}
+
+void do_work(std::vector<double>& _x, std::vector<double>& _y, bool& abort, std::shared_mutex& _mutex) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        std::unique_lock lock(_mutex);
+        _x.push_back(_x.size());
+        _y.push_back(_y.size());
+        if (abort)
+            break;
+    };
+}
+
+int main(int, char**)
+{
+    auto imgui_context=initialise();
+    //ImPlot::GetStyle().AntiAliasedLines = true;
 
     // Our state
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    std::vector<double> xs;
+    std::vector<double> y;
+    bool abort = false;
+    std::shared_mutex mutex_;
+
+    std::thread t(do_work, std::ref(xs), std::ref(y), std::ref(abort), std::ref(mutex_));
+    
     // Main loop
     bool done = false;
+    bool updatePlot=true;
+    bool autoScale = true;
     while (!done)
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    {        
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
             ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
                 done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(imgui_context.window))
                 done = true;
         }
 
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplSDL2_NewFrame(window);
+        ImGui_ImplSDL2_NewFrame(imgui_context.window);
         ImGui::NewFrame();
 
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        //ImGui::GetStyle().WindowMinSize.x = 400;
+        //ImGui::GetStyle().WindowMinSize.y = 400;       
+        ImGui::SetNextWindowSizeConstraints(ImVec2(400, 400), ImVec2(10240, 10240));
+        ImGui::Begin("Plot");
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
+            ImGui::Checkbox("Update plot", &updatePlot);
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::Checkbox("Autoscale", &autoScale);            
+            ImGui::Text("Number of elements: %llu", xs.size());
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+            ImPlot::SetNextPlotLimits(0, xs.back() + 10, 0, y.back() + 10,
+                                      autoScale ? ImGuiCond_::ImGuiCond_Always
+                                                : ImGuiCond_::ImGuiCond_Once);
+            if (ImPlot::BeginPlot("Log Plot", NULL, NULL, ImVec2(-1, -1),
+                                  ImGuiCond_::ImGuiCond_Always)) {
+                std::shared_lock lock(mutex_);
+                ImPlot::PlotLine("f(x) = x", xs.data(), y.data(),
+                                 (int)xs.size());
+                ImPlot::EndPlot();
+            }
         }
+        ImGui::End();
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        ImGui::Begin("Mis");
+        done = ImGui::Button("Exit");
+        ImGui::End();
 
         // Rendering
         ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glViewport(0, 0, (int)imgui_context.io.DisplaySize.x, (int)imgui_context.io.DisplaySize.y);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(imgui_context.window);
     }
 
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
+    {
+        std::shared_lock lock(mutex_);
+        abort = true;
+    }
+    t.join();
 
-    SDL_GL_DeleteContext(gl_context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    clean_up(imgui_context);
 
     return 0;
 }
