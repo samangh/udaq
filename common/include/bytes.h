ae7582af76b9074c6441ed3f117e0f20312779a9
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-03-26 08:37:30 +0000

    common: add bytes functions

--- common/include/bytes.h
+++ common/include/bytes.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cstdint>
-
+#include <type_traits>
 namespace udaq::common::bytes {
 
 enum class Endianess
@@ -15,5 +15,10 @@ static uint32_t to_uint32(const uint8_t *buff, Endianess endian = Endianess::Lit
 static int32_t to_int32(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
 static uint64_t to_uint64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
 static int64_t to_int64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+static double to_double(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+
+template <typename T>
+static T swap_endian(const T val, typename std::enable_if<std::is_arithmetic<T>::value, std::nullptr_t>::type = nullptr);
+
 
 }
--- common/src/bytes.cpp
+++ common/src/bytes.cpp
@@ -1,18 +1,92 @@
 #include <bytes.h>
+#include <type_traits>
+#include <array>
+
+#include <algorithm>
+
+template<typename T> T shift(const uint8_t *buff, unsigned int no_bytes, udaq::common::bytes::Endianess endian)
+{
+    T result = 0;
+    if (endian == udaq::common::bytes::Endianess::LittleEndian)
+        for(unsigned int i = no_bytes-1; i >= 0; --i )
+        {
+            result <<= 8;
+            result |= (T)buff[i];
+        }
+    else
+        for(unsigned int i = 0; i >= no_bytes-1; ++i )
+        {
+            result <<= 8;
+            result |= (T)buff[i];
+        }
+    return result;
+}
+
+udaq::common::bytes::Endianess get_system_endianess(void)
+{
+    union {
+        uint32_t i;
+        char c[4];
+    } u = {0x01020304};
+
+    if (u.c[0] == 1)
+        return udaq::common::bytes::Endianess::BigEndian;
+    else
+        return udaq::common::bytes::Endianess::LittleEndian;
+}
 
 uint16_t udaq::common::bytes::to_uint16(const uint8_t *buff, Endianess endian)
 {
-    if (endian == Endianess::LittleEndian)
-        return buff[0] | buff[1] << 8;
-    else
-        return buff[1] | buff[0] << 8;
+    return shift<uint16_t>(buff, 2, endian);
 }
 
 uint32_t udaq::common::bytes::to_uint32(const uint8_t *buff, udaq::common::bytes::Endianess endian)
 {
-    if (endian == Endianess::LittleEndian)
-        return buff[0] | buff[1] << 8 | buff[2] << 16 | buff[3] << 24;
-    else
-        return buff[3] | buff[2] << 8 | buff[1] << 16 | buff[0] << 24;
+    return shift<uint32_t>(buff, 4, endian);
+}
 
+int32_t udaq::common::bytes::to_int32(const uint8_t *buff, udaq::common::bytes::Endianess endian)
+{
+    return shift<int32_t>(buff, 4, endian);
+}
+
+uint64_t udaq::common::bytes::to_uint64(const uint8_t *buff, udaq::common::bytes::Endianess endian)
+{
+    return shift<uint64_t>(buff, 8, endian);
+}
+
+
+int64_t udaq::common::bytes::to_int64(const uint8_t *buff, udaq::common::bytes::Endianess endian)
+{
+    return shift<int64_t>(buff, 8, endian);
+}
+
+double udaq::common::bytes::to_double(const uint8_t *buff, udaq::common::bytes::Endianess endian)
+{
+    double r;
+    std::copy(buff, buff + sizeof(double), reinterpret_cast<char*>(&r));
+
+    /* swap endianess if the storage buffer has different endianess to the system */
+    if (get_system_endianess() != endian)
+        r=swap_endian(r);
+
+    return r;
+}
+
+template<typename T>
+T udaq::common::bytes::swap_endian(const T val, typename std::enable_if<std::is_arithmetic<T>::value, std::nullptr_t>::type)
+{
+    // Taken from
+    // https://mklimenko.github.io/english/2018/08/22/robust-endian-swap/
+    
+    // With optimisations enabled, this will use a single 'BSWAP'
+    // instruction to swap endianess
+    union U {
+        T val;
+        std::array<std::uint8_t, sizeof(T)> raw;
+    } src, dst;
+
+    src.val = val;
+    std::reverse_copy(src.raw.begin(), src.raw.end(), dst.raw.begin());
+    return  dst.val;
 }
