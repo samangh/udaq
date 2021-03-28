e8635d7161fb34185c8a61bd83c85c620e2ffabc
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-03-28 02:09:16 +0100

    common: fix 'bytes' functions

--- common/include/bytes.h
+++ common/include/bytes.h
@@ -10,12 +10,12 @@ enum class Endianess
     BigEndian
 };
 
-static uint16_t to_uint16(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
-static uint32_t to_uint32(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
-static int32_t to_int32(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
-static uint64_t to_uint64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
-static int64_t to_int64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
-static double to_double(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+uint16_t to_uint16(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+uint32_t to_uint32(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+int32_t to_int32(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+uint64_t to_uint64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+int64_t to_int64(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
+double to_double(const uint8_t *buff, Endianess endian = Endianess::LittleEndian);
 
 template <typename T>
 static T swap_endian(const T val, typename std::enable_if<std::is_arithmetic<T>::value, std::nullptr_t>::type = nullptr);
