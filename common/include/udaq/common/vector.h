e01a0c02b11a5e41e8fe389c1864666ea9c8a739
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-03-31 11:31:50 +0100

    common: add vector lower/upper bound index functions

--- common/include/udaq/common/vector.h
+++ common/include/udaq/common/vector.h
@@ -12,4 +12,18 @@ void append(std::vector<T>& base, const std::vector<T> to_add)
     base.insert(std::end(base), std::begin(to_add), std::end(to_add));
 }
 
+template<typename T>
+int upper_bound_index(const std::vector<T>& data, T  value)
+{
+    auto upper = std::upper_bound(data.begin(), data.end(), value);
+    return  std::distance(data.begin(), upper);
+}
+
+template<typename T>
+int lower_bound_index(const std::vector<T>& data, double  value)
+{
+    auto upper = std::lower_bound(data.begin(), data.end(), value);
+    return std::distance(data.begin(), upper);
+}
+
 }
