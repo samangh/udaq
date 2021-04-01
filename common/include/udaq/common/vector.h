863bdcb1ca238fb635edf22584a0e1b01d6ae780
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-01 11:00:00 +0100

    common: bug in vector::lower_bound_index and upper_bound_index

--- common/include/udaq/common/vector.h
+++ common/include/udaq/common/vector.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <type_traits>
+#include <memory>
 
 namespace udaq::common::vector {
 
@@ -12,18 +13,29 @@ void append(std::vector<T>& base, const std::vector<T> to_add)
     base.insert(std::end(base), std::begin(to_add), std::end(to_add));
 }
 
-template<typename T>
-int upper_bound_index(const std::vector<T>& data, T  value)
+template <template <typename, typename> class Container,
+    typename Value,
+    typename Allocator = std::allocator<Value>>
+int upper_bound_index(const Container<Value, Allocator>& data, const Value& to_find)
 {
-    auto upper = std::upper_bound(data.begin(), data.end(), value);
-    return  std::distance(data.begin(), upper);
+    auto upper = std::upper_bound(data.begin(), data.end(), to_find);
+    if (upper == data.end())
+        return data.size() - 1;
+    else
+        return  std::distance(data.begin(), upper);
 }
 
-template<typename T>
-int lower_bound_index(const std::vector<T>& data, double  value)
+template <template <typename, typename> class Container,
+    typename Value,
+    typename Allocator = std::allocator<Value>>int
+ lower_bound_index(const Container<Value, Allocator>& data, const Value& to_find)
 {
-    auto upper = std::lower_bound(data.begin(), data.end(), value);
-    return std::distance(data.begin(), upper);
+    auto a = data.begin();
+    auto upper = std::lower_bound(data.begin(), data.end(), to_find);
+    if (upper == data.end())
+        return data.size() - 1;
+    else
+        return std::distance(data.begin(), upper);
 }
 
 }
