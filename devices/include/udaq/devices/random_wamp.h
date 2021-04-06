f19943bf6df2561032b0594370d5ccd05cd4822d
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-06 12:28:09 +0100

    Remove wampcc for now

--- devices/include/udaq/devices/random_wamp.h
+++ devices/include/udaq/devices/random_wamp.h
@@ -1,13 +1,13 @@
-#include <udaq/types.h>
+// #include <udaq/types.h>
 
-namespace udaq::devices
-{
-    class random_wamp
-    {
-    public:
-        random_wamp(std::string router_addr, port_t port);
-        void connect_client();
-        void disconnect_client();
-    private:
-    };
-}
+// namespace udaq::devices
+// {
+//     class random_wamp
+//     {
+//     public:
+//         random_wamp(std::string router_addr, port_t port);
+//         void connect_client();
+//         void disconnect_client();
+//     private:
+//     };
+// }
