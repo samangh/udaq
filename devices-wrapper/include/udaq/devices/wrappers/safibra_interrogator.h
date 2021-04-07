9092dc1b7d9dbab578047ed4740f499fa63d7bfe
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-07 23:27:08 +0100

    Use cmake to generate export headers

--- devices-wrapper/include/udaq/devices/wrappers/safibra_interrogator.h
+++ devices-wrapper/include/udaq/devices/wrappers/safibra_interrogator.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <udaq/devices/wrappers/globals.h>
+#include <udaq/export/devices-wrapper.h>
 
-EXTERN_C_START
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct safibra_packet safibra_packet;
 typedef struct safibra_packet_buffer safibra_packet_buffer;
@@ -35,19 +38,22 @@ typedef void (*safibra_stopped_listening_cb)(void);
 typedef void (*safibra_data)(safibra_packet_buffer buffer);
 
 /* Creates a new client for Safibra FBG interrogators. The resultign client must be freed by calling safibra_free_client() afterards. */
-DLL_PUBLIC safibra_client safibra_create_client(safibra_error_cb_t erro_cb,
+DEVICES_WRAPPER_EXPORT safibra_client safibra_create_client(safibra_error_cb_t erro_cb,
                                        safibra_connected_cb client_connected_cb,
                                        safibra_disconnected_cb client_disconnected_cb,
                                        safibra_started_listening_cb started_listening_cb,
                                        safibra_stopped_listening_cb stopped_listening_cb,
                                        safibra_data data_available_cb);
 
-DLL_PUBLIC void safibra_start(safibra_client client, int port);
-DLL_PUBLIC void safibra_stop(safibra_client client);
-DLL_PUBLIC bool safibra_is_running(safibra_client client);
+DEVICES_WRAPPER_EXPORT void safibra_start(safibra_client client, int port);
+DEVICES_WRAPPER_EXPORT void safibra_stop(safibra_client client);
+DEVICES_WRAPPER_EXPORT bool safibra_is_running(safibra_client client);
 
-DLL_PUBLIC void safibra_free_buffer(safibra_packet_buffer buffer);
-DLL_PUBLIC void safibra_free_client(safibra_client client);
+DEVICES_WRAPPER_EXPORT void safibra_free_buffer(safibra_packet_buffer buffer);
+DEVICES_WRAPPER_EXPORT void safibra_free_client(safibra_client client);
 
-DLL_PUBLIC int safibra_number_of_clients(safibra_client client);
-EXTERN_C_END
+DEVICES_WRAPPER_EXPORT int safibra_number_of_clients(safibra_client client);
+
+#ifdef __cplusplus
+}
+#endif
