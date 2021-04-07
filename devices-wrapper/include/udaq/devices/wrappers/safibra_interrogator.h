98fc31f0f32c10aebec9ceb974ff74ffd10d4412
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-07 01:26:01 +0100

    WIP: devices-wrapper: add safibra wrapper

--- devices-wrapper/include/udaq/devices/wrappers/safibra_interrogator.h
+++ devices-wrapper/include/udaq/devices/wrappers/safibra_interrogator.h
@@ -15,15 +15,19 @@ struct safibra_r {
 	size_t length;
 };
 
-typedef void (*safibra_error_cb_t)(const char* msg, int size);
-typedef void (*safibra_connected_cb)(void);
-typedef void (*safibra_disconnected_cb)(void);
-typedef void (*safibra_started_listening_cb)(void);
-typedef void (*safibra_stopped_listening_cb)(void);
-typedef void (*safibra_data)(safibra_r* buffer, size_t length);
+typedef void (safibra_error_cb_t)(const char* msg, int size);
+typedef void (safibra_connected_cb)(void);
+typedef void (safibra_disconnected_cb)(void);
+typedef void (safibra_started_listening_cb)(void);
+typedef void (safibra_stopped_listening_cb)(void);
+typedef void (safibra_data)(safibra_r* buffer, size_t length);
 
-DLL_PUBLIC void* safibra_create_client(safibra_error_cb_t* erro_cb, safibra_connected_cb* client_connected_cb,
-	safibra_disconnected_cb* client_disconnected_cb, safibra_started_listening_cb* started_listening_cb, safibra_data* data_available_cb);
+DLL_PUBLIC void* safibra_create_client(safibra_error_cb_t* erro_cb,
+                                       safibra_connected_cb* client_connected_cb,
+                                       safibra_disconnected_cb* client_disconnected_cb,
+                                       safibra_started_listening_cb* started_listening_cb,
+                                       safibra_stopped_listening_cb* stopped_listening_cb,
+                                       safibra_data* data_available_cb);
 
 DLL_PUBLIC void safibra_start(void* client, const int port);
 DLL_PUBLIC void safibra_stop(void* client);
--- tests/device-wrapper/src/main.c
+++ tests/device-wrapper/src/main.c
@@ -1,4 +1,5 @@
 #include <udaq/devices/wrappers/safibra_interrogator.h>
+#include <unistd.h>
 
 void on_error(const char* msg, int size) {
 
@@ -6,17 +7,18 @@ void on_error(const char* msg, int size) {
 
 void on_data(safibra_r* buffer, size_t length)
 {
-
+    printf("data\n");
 };
 
 void empty()
 {
-
 };
 
 int main(void)
 {
-    void* client = safibra_create_client(&on_error, &empty, &empty, &empty, &on_data);
+    void* client = safibra_create_client(&on_error, &empty, &empty, &empty, &empty, &on_data);
     safibra_start(client, 5555);
+    for (;;)
+        sleep(50);
     return 0;
 };
