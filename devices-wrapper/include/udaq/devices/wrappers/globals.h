8eadc5363cfd9be659d42ffbfda3e64399e2fbc7
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-07 12:20:14 +0100

    devices-wrapper: tidy up exports

--- devices-wrapper/include/udaq/devices/wrappers/globals.h
+++ devices-wrapper/include/udaq/devices/wrappers/globals.h
@@ -24,21 +24,17 @@
 #endif
 
 
-#if defined _WIN32 || defined __CYGWIN__
-  #ifdef DEVICES_WRAPPER_BUILDING_DLL
-    #ifdef __GNUC__
-     #define DLL_PUBLIC EXTERN_C __attribute__((dllexport))
-    #else
-      #define DLL_PUBLIC __declspec(dllexport)
-    #endif
+#ifdef DEVICES_WRAPPER_BUILDING_DLL
+  #ifdef _MSC_VER
+    #define DLL_PUBLIC __declspec(dllexport)
   #else
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllimport))
-    #else
-      #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-    #endif
+    #define DLL_PUBLIC EXTERN_C __attribute__((dllexport))
   #endif
 #else
-    #define DLL_PUBLIC
+  #ifdef _MSC_VER
+    #define DLL_PUBLIC __declspec(dllimport)
+  #else
+    #define DLL_PUBLIC __attribute__ ((dllimport))
+  #endif
 #endif
 
