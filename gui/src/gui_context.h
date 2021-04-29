4b7a3c8536871a4c2cb510988f5d38806a9d145b
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-04-29 20:21:47 +0100

    Tidy up GUI

---
+++ gui/src/gui_context.h
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <atomic>
+#include <functional>
+#include <imgui-wrapper.h>
+#include <implot.h>
+
+#include "karla-font.h"
+
+
+class MyContext {
+
+public:
+    ImGuiIO io;
+    SDL_Window *window;
+    void initialise(const std::string& title){
+        cleaned_up =false;
+
+        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+        {
+            std::cout << "Error: " << SDL_GetError() << std::endl;
+            exit(-1);
+        }
+
+    #ifdef __APPLE__
+        // GL 3.2 Core + GLSL 150
+        const char* glsl_version = "#version 150";
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    #else
+        // GL 3.0 + GLSL 130
+        const char* glsl_version = "#version 130";
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    #endif
+
+        // Create window with graphics context
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+        window = SDL_CreateWindow(title.c_str(),  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+        gl_context = SDL_GL_CreateContext(window);
+        SDL_GL_MakeCurrent(window, gl_context);
+
+        SDL_GL_SetSwapInterval(1); // Enable vsync
+
+        // Initialize OpenGL loader
+        if (gl3wInit() != 0)
+        {
+            fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+            exit(-1);
+        }
+
+         // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImPlot::CreateContext();
+        io = ImGui::GetIO();
+        (void)io;
+
+        ImGui::GetIO().Fonts->AddFontFromMemoryCompressedTTF(
+            Karla_compressed_data, karla_compressed_size, 14);
+        io.Fonts->Build();
+
+        ImGui::StyleColorsDark();
+        ImGui::GetStyle().WindowRounding = 5.0f;
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+    }
+    void start_gui_loop(std::function<void(void)> func)
+    {
+       gui_done = false;
+
+       while (!gui_done)
+       {
+           SDL_Event event;
+           while (SDL_PollEvent(&event))
+           {
+               ImGui_ImplSDL2_ProcessEvent(&event);
+               if (event.type == SDL_QUIT)
+                   gui_done = true;
+               if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                   gui_done = true;
+           }
+
+           // Start the Dear ImGui frame
+           ImGui_ImplOpenGL3_NewFrame();
+           ImGui_ImplSDL2_NewFrame(window);
+
+           func();
+
+           // Rendering
+           ImGui::Render();
+           glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+           glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+           glClear(GL_COLOR_BUFFER_BIT);
+           ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+           SDL_GL_SwapWindow(window);
+       }
+
+    }
+
+    /* Stops the GUI loop. This is not thread safe, it should only be called within the function passed to start_gui_loop().*/
+    void stop_gui_loop()
+    {
+        gui_done=false;
+    }
+
+    void cleanup() {
+        // Cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplSDL2_Shutdown();
+
+        ImPlot::DestroyContext();
+        ImGui::DestroyContext();
+
+        SDL_GL_DeleteContext(gl_context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        cleaned_up = true;
+    }
+
+    ~MyContext(){
+        if (!cleaned_up)
+            cleanup();
+    }
+
+private:
+    bool gui_done;
+    std::atomic<bool> cleaned_up;
+    SDL_GLContext gl_context;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+};
--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,129 +1,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <version.h>
+#include <atomic>
 
 #include <imgui-wrapper.h>
 #include <imgui_internal.h>
 #include <imfilebrowser.h>
-
 #include <implot.h>
-#include <cstdlib>
-#include <version.h>
-#include "karla-font.h"
-
-#include <atomic>
-#include <chrono>
-#include <thread>
-#include <mutex>
-#include <shared_mutex>
-#include <algorithm>
-
-#include <udaq/devices/safibra/sigproc_server.h>
-#include <udaq/common/vector.h>
-#include <udaq/common/file_writer.h>
-#include <udaq/helpers/imgui.h>
-
-#include <cmath>
 #include <fmt/format.h>
 
-class MyContext {
-  public:
-    SDL_Window *window;
-    SDL_GLContext gl_context;
-    ImGuiIO io;
-};
+#include <udaq/helpers/imgui.h>
 
-void clean_up(MyContext &context) {
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-
-    ImPlot::DestroyContext();
-    ImGui::DestroyContext();
-
-    SDL_GL_DeleteContext(context.gl_context);
-    SDL_DestroyWindow(context.window);
-    SDL_Quit();
-}
-
-MyContext initialise() {
-    MyContext imgui_context;
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
-    {
-        std::cout << "Error: " << SDL_GetError() << std::endl;
-        exit(-1);
-    }
-
-#ifdef __APPLE__
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = "#version 150";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
-
-    // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    imgui_context.window = SDL_CreateWindow("uDAQ",  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    imgui_context.gl_context = SDL_GL_CreateContext(imgui_context.window);
-    SDL_GL_MakeCurrent(imgui_context.window, imgui_context.gl_context);
-
-    SDL_GL_SetSwapInterval(1); // Enable vsync
-
-    // Initialize OpenGL loader
-    if (gl3wInit() != 0)
-    {
-        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-        exit(-1);
-    }
-
-     // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImPlot::CreateContext();
-   imgui_context.io = ImGui::GetIO();
-      (void)imgui_context.io;
-
-    ImGui::GetIO().Fonts->AddFontFromMemoryCompressedTTF(
-        Karla_compressed_data, karla_compressed_size, 14);
-    imgui_context.io.Fonts->Build();
-
-    ImGui::StyleColorsDark();
-    ImGui::GetStyle().WindowRounding = 5.0f;
-
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForOpenGL(imgui_context.window, imgui_context.gl_context);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    return imgui_context;
-}
-
-void disable_item(bool visible, std::function<void(void)> func)
-{
-    if (visible)
-    {
-        ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
-        ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
-        func();
-        ImGui::PopItemFlag();
-        ImGui::PopStyleVar();
-    }
-    else
-        func();
-}
+#include "gui_context.h"
 
 #ifdef WIN32
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
@@ -132,39 +22,18 @@ void disable_item(bool visible, std::function<void(void)> func)
 int main(int, char**)
 {
 
-    auto imgui_context=initialise();
-    //ImPlot::GetStyle().AntiAliasedLines = true;
+    MyContext imgui_context;
+    imgui_context.initialise("uDAQ");
 
     // Our state
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-
-    // Main loop
-    bool done = false;
-
-    while (!done)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(imgui_context.window))
-                done = true;
-        }
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplSDL2_NewFrame(imgui_context.window);
+    imgui_context.start_gui_loop([&imgui_context](){
         ImGui::NewFrame();
-
         
-
         //Menu bar
         if (ImGui::BeginMainMenuBar()) {
             if (ImGui::BeginMenu("File")) {
-                done = ImGui::MenuItem("Exit");
+                if (ImGui::MenuItem("Exit"))
+                    imgui_context.stop_gui_loop();
                 ImGui::EndMenu();
             }
 
@@ -174,16 +43,9 @@ int main(int, char**)
             ImGui::EndMainMenuBar();
         }
 
-        // Rendering
-        ImGui::Render();
-        glViewport(0, 0, (int)imgui_context.io.DisplaySize.x, (int)imgui_context.io.DisplaySize.y);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(imgui_context.window);
-    }
+    });
 
-    clean_up(imgui_context);
+    imgui_context.cleanup();
 
     return 0;
 }
