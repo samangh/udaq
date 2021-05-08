e26d57d0db5ba2cf3ca6a023515b76f853b73243
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2021-05-08 19:05:55 +0100

    Add command line options to safibra-simulator

--- tools/safibra-simulator/src/main.cpp
+++ tools/safibra-simulator/src/main.cpp
@@ -1,29 +1,28 @@
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
 
+#include <atomic>
+#include <chrono>
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
-
-#include <boost/bind.hpp>
-#include <boost/asio.hpp>
-#include <boost/thread.hpp>
-
-#include <thread>
-#include <chrono>
-
-#include <ctime>
-#include <bytes.h>
-
-#include <cmath>
-#include <atomic>
-#include <udaq/common/accurate_sleeper.h>
 #include <vector>
 
-using boost::asio::ip::tcp;
-static std::atomic<bool> async_writing_in_progress;
+#include <CLI/CLI.hpp>
+#include <boost/asio.hpp>
+
+#include <bytes.h>
+#include <udaq/common/accurate_sleeper.h>
+
 static boost::asio::io_service io_service;
 
+/* True if async write in progress */
+static std::atomic<bool> async_writing_in_progress;
+
+/* Buffer to store data to send */
 static std::vector<unsigned char> buffer;
+
+/* Buffer that is passed to ASIO for async write */
 static std::vector<unsigned char> buffer_async;
 
 uint32_t compute_checksum(std::vector<unsigned char>& message, int message_size) {
@@ -40,19 +39,13 @@ uint32_t compute_checksum(std::vector<unsigned char>& message, int message_size)
 }
 
 template <typename T>
-void add_to_byte_array(std::vector<uint8_t>& array, T to_add, typename std::enable_if<std::is_integral<T>::value, std::nullptr_t>::type = nullptr)
+void add_to_byte_array(std::vector<uint8_t>& array, T to_add)
 {
     auto bytes = udaq::common::bytes::to_bytes(to_add, udaq::common::bytes::Endianess::LittleEndian);
     array.insert(array.end(), bytes.begin(), bytes.end());
 }
 
-void add_to_byte_array(std::vector<uint8_t>& array, double to_add)
-{
-    auto bytes = udaq::common::bytes::to_bytes(to_add, udaq::common::bytes::Endianess::LittleEndian);
-    array.insert(array.end(), bytes.begin(), bytes.end());
-}
-
-void asio_work_done(const boost::system::error_code& ec, std::size_t bytes_transferred)
+void asio_work_done(const boost::system::error_code& ec, std::size_t)
 {
     async_writing_in_progress = false;
     buffer_async.clear();
@@ -61,16 +54,16 @@ void asio_work_done(const boost::system::error_code& ec, std::size_t bytes_trans
         throw std::runtime_error(ec.message());
 }
 
-void write_data(boost::asio::ip::tcp::socket& socket)
+void write_data(boost::asio::ip::tcp::socket& socket, int readings_per_packet, int no_sensors, double sample_rate)
 {
-    using namespace std::chrono_literals;
-
-    /* number of readouts per packet*/
-    int n = 20;
-
-    /* Set rate to 125 Hz */
+    /* Set rate */
+    int internval_ns =  1E9 / sample_rate;
     udaq::common::AccurateSleeper sleeper;
-    sleeper.set_interval(8ms);
+    sleeper.set_interval(internval_ns);
+
+    std::vector<std::string> sensor_names;
+    for (int i=0; i < no_sensors; i++)
+        sensor_names.push_back("sensor"+std::to_string(i));
 
     /* i is sequence number */
     for (int i = 0; ; ++i)
@@ -79,7 +72,7 @@ void write_data(boost::asio::ip::tcp::socket& socket)
         std::vector<uint64_t> microseconds;
         std::vector<double> wavelengths;
 
-        for (int k = 0; k < n; ++k)
+        for (int k = 0; k < readings_per_packet; ++k)
         {
             sleeper.sleep();
             auto t = std::chrono::high_resolution_clock::now().time_since_epoch();
@@ -91,16 +84,16 @@ void write_data(boost::asio::ip::tcp::socket& socket)
             wavelengths.push_back(sin(std::chrono::duration_cast<std::chrono::microseconds>(t).count() * 1E-6 * 3.14 /5));
         }
 
-        for (std::string sensor_id : {"sensor0", "sensor1", "sensor2", "sensor3", "sensor4", "sensor5", "sensor6"})
+        for (const auto& sensor_name : sensor_names)
         {
             std::string device_id = "simulated_device0";
-            auto device_id_padded = device_id.append(std::string((32 - strlen(device_id.c_str())), '\0')).c_str();
+            auto sensor_id = sensor_name;
 
-            //std::string sensor_id = "simulated_sensor0";
+            auto device_id_padded = device_id.append(std::string((32 - strlen(device_id.c_str())), '\0')).c_str();
             auto sensor_id_padded = sensor_id.append(std::string((32 - strlen(sensor_id.c_str())), '\0')).c_str();
 
             /* packet size */
-            int packet_size = 80 + n * 24 + 4;
+            int packet_size = 80 + readings_per_packet * 24 + 4;
 
             std::vector<unsigned char> saf_header;
             /* SYNC */
@@ -116,7 +109,7 @@ void write_data(boost::asio::ip::tcp::socket& socket)
             saf_header.insert(saf_header.end(), sensor_id_padded, sensor_id_padded + 32);
 
             add_to_byte_array(saf_header, (uint16_t)i); /* sequenc no */
-            add_to_byte_array(saf_header, (uint16_t)n); /* number of readouts */
+            add_to_byte_array(saf_header, (uint16_t)readings_per_packet); /* number of readouts */
 
             add_to_byte_array(saf_header, (uint32_t)packet_size);
             add_to_byte_array(saf_header, (uint32_t)compute_checksum(saf_header, 80));
@@ -141,37 +134,41 @@ void write_data(boost::asio::ip::tcp::socket& socket)
             io_service.restart();
             io_service.run_one();            
         }
-
-
-
     }
 }
 
 int main(int argc, char* argv[])
 {
+    CLI::App app{"Safibra FBG interrogator simulator. This program will stream data in the same format as a physical Safibra FBG interrogator."};
+
+    unsigned int readings_per_packet;
+    std::string ip;
+    unsigned int port;
+    unsigned int no_sensors;
+    double sample_rate;
+
+    app.add_option("--ip", ip, "IP address to stream data to")->required();
+    app.add_option("--port", port, "port number to connect to")->required();
+    app.add_option("--sensors", no_sensors, "number of sensors to simulate")->default_val(6);
+    app.add_option("--readings-per-packet", readings_per_packet, "number of redings per packet for a single FBG")->default_val(10);
+    app.add_option("--rate", sample_rate, "sample rate in Hz (approximate)")->default_val(125);
+
+    CLI11_PARSE(app, argc, argv);
+
     try
     {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: safibra-simulator <host> <port>\n";
-            return 1;
-        }
-
-        auto host = boost::asio::ip::address::from_string(argv[1]);
-        int port = atoi(argv[2]);
-
+        auto host = boost::asio::ip::address::from_string(ip);
         boost::asio::ip::tcp::endpoint endpoint(host, port);
-
         boost::asio::ip::tcp::socket socket(io_service);
 
         socket.connect(endpoint);
-        write_data(socket);
+        write_data(socket, readings_per_packet, no_sensors, sample_rate);
         socket.close();
-
     }
     catch (const std::exception& e)
     {
-        std::cerr << "Exception: " << e.what() << "\n";
+        std::cerr << "Error: " << e.what() << "\n";
+        return -1;
     }
     return 0;
 }
